6f5a1e9e3d9ddebd3ab2b1fa57bba93defa2bd02
Leandro Gomes Breve <45273595+leandrogbreve@users.noreply.github.com>
2026-04-29 18:23:10 -0300

    correção no worker

--- packages/shared/nistiprint_shared/services/redis_queue_tasks.py
+++ packages/shared/nistiprint_shared/services/redis_queue_tasks.py
@@ -87,9 +87,10 @@ def log_shared_task_execution(task_type: str = None):
 
 
 # Configuração do Redis
-REDIS_HOST = 'redis'
-REDIS_PORT = 6379
-REDIS_DB = 0
+import os
+REDIS_HOST = os.environ.get('REDIS_HOST', 'redis')
+REDIS_PORT = int(os.environ.get('REDIS_PORT', 6379))
+REDIS_DB = int(os.environ.get('REDIS_DB', 0))
 
 # Filas
 BLING_WEBHOOK_QUEUE = 'bling:webhooks:pendentes'
