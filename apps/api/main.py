14da110e332c90c6d75ffc22beb1d213ffb68586
Leandro Gomes Breve <45273595+leandrogbreve@users.noreply.github.com>
2026-04-29 15:40:52 -0300

    tirar api da internet

--- apps/api/main.py
+++ apps/api/main.py
@@ -269,7 +269,7 @@ if __name__ == "__main__":
     port = int(os.environ.get("PORT", 8080))
     # In production, debug should be False.
     debug_mode = os.environ.get('FLASK_ENV') == 'development'
-    app.run(host='0.0.0.0', port=port, debug=debug_mode)
+    app.run(host='127.0.0.1', port=port, debug=debug_mode)
 
 
 
